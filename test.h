3174219639dbc439d38ed5deb58e0cc0b1bf202c
zyjiang <zyjiang@aaaa.com>
2022-01-26 15:46:38 +0800

    ready to build AST

--- test.h
+++ test.h
@@ -1,5 +1,36 @@
-extern int yylineno; /* from lexer */
-void yyerror( const char *s, ...);
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef enum NodeTag
+{
+    T_Stmt,
+    T_Value
+}NodeTag;
+
+typedef struct Node
+{
+    NodeTag type;
+}Node;
+
+#define newNode(size, tag) \
+({                      \
+     Node *_result;  \
+     assert((size) >= sizeof(Node));    /* 检测申请的内存大小，>>=sizeof(Node) */ \
+     _result = (Node *) malloc(size);   /* 申请内存 */ \
+     _result->type = (tag);             /*设置TypeTag */ \
+     _result;                   /*返回值*/\
+})
+
+#define makeNode(_type_) ((_type_ *)newNode(sizeof(_type_),T_##_type_))
+
+#define nodeTag(nodeptr) (((const Node *)(nodeptr))->type)
+
+
+
 
 /* nodes in the Abstract Syntax Tree */
 struct ast {
@@ -12,6 +43,12 @@ struct numval {
   int nodetype;         /* type K */
   double number;
 };
+
+
+
+
+extern int yylineno; /* from lexer */
+void yyerror( const char *s, ...);
 
 /* build an AST */
 struct ast *newast(int nodetype, struct ast *l, struct ast *r);
--- test.y
+++ test.y
@@ -1,8 +1,4 @@
 %{
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdarg.h>
-#include<string.h>
 #include"test.h"
 
 void emit(char *s, ...);

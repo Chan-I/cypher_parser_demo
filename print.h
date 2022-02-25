f92bc868054a896aa30ce1b66e81506b3224b007
jywang <jywang@ceresdata.com>
2022-02-25 17:31:27 +0800

    print whereclause

--- print.h
+++ print.h
@@ -11,4 +11,17 @@ void WhereStmtPrint(WhereStmtClause *wh, char *sql);
 
 void MatchStmtPrint(MatchStmtClause *mch, char *sql);
 
+void PrintCES(ComparisionExpr_Stru *ces, char *sql);
+
+void PrintCS(Comparision_Stru *cs, char *sql);
+
+void PrintLT(LiteralType *lt, char *sql);
+
+void PrintAE(AnyExpr *ae, char *sql);
+
+void PrintISAList(List *l, char *sql);
+
+void PrintSCE(SubCompExpr *sce, char *sql);
+
+
 #endif

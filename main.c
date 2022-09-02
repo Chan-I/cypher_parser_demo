37931a49796228cb14f4d00066408dd93ef7621d
Chan-i Chiang <jzy12081003@163.com>
2022-09-02 16:50:29 +0800

    update main file to raw_parser

--- main.c
+++ main.c
@@ -6,50 +6,9 @@ int main(int argc, char **argv)
 	int res;
 	char *sql = NULL;
 
-#if USE_PARSE_MODULE
-	/**
-	 * @brief  There are two APIs.
-	 * 			one is new_module_from_stdin()...
-	 * 				   parse_module()
-	 *
-	 * 			the other is raw_parser()
-	 *
-	 */
-	while (1)
-	{
-		fprintf(stdout, "CypherParser=# ");
-		fflush(stdout);
-		mod = new_module_from_stdin();
-		res = parse_module(mod);
-		/**
-		 * @brief	the first usage:
-		 * 						new_module_from_[stdin/string/file]();
-		 * 			   			print_module();
-		 *
-		 */
-
-		if (!res) // success
-		{
-			printf("success!\n");
-			sql = print_module(mod);
-			// printf("%s\n", sql);
-		}
-		else if (res == 1)
-		{
-			continue;
-		}
-		else
-			exit(0);
-		if (sql)
-			free(sql);
-		delete_module(mod);
-		fprintf(stdout, "\n\n");
-		fflush(stdout);
-	}
-#else
 	char *cypher = "MATCH (david {name: 'David'})--(otherPerson)-->() "
 				   "WITH otherPerson, count(*) AS foaf WHERE foaf > 1 "
-				   "RETURN otherPerson.name ;";
+				   "RETURN otherPerson.name V;";
 
 	fprintf(stdout, "CypherParser=# %s\n\n", cypher);
 	fflush(stdout);
@@ -76,6 +35,5 @@ int main(int argc, char **argv)
 
 	fprintf(stdout, "\n\n");
 	fflush(stdout);
-#endif
 	return res;
 }
